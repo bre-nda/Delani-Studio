d17f2f6ce0edc715d9e8258ba1716012c352b0d2
bre-nda <brenda.kanale@student.moringaschool.com>
2021-07-25 15:43:52 +0300

    add mouse over function

--- index.js
+++ index.js
@@ -42,3 +42,17 @@ $(document).ready(function() {
     });
 
 });
+$(document).ready(function() {
+    $("#work1").mouseover(function() {
+        $("#overlay").show();
+    }).mouseout(function() {
+        $("#overlay").hide();
+    });
+});
+$(document).ready(function() {
+    $("#work2").mouseover(function() {
+        $("#overlay2").show();
+    }).mouseout(function() {
+        $("#overlay2").hide();
+    });
+});
